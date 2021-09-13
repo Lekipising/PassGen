a44cf5ceab60ba10a901b7d298dd46d18c954496
lekipising <leeplanke@gmail.com>
2021-09-13 23:22:19 +0200

    icon change and selection

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,18 +1,45 @@
 $(document).ready(function () {
-  $(".minus").click(function () {
+  $(".minu").click(function () {
     var lengthdw = document.getElementById("length");
     var val = parseInt(lengthdw.value) - 1;
-    let count = val
+    let count = val;
     count = count < 8 ? 8 : count;
     lengthdw.value = count;
     return false;
   });
-  $(".plus").click(function () {
+  $(".plu").click(function () {
     var lengthup = document.getElementById("length");
     var val = parseInt(lengthup.value) + 1;
-    let count = val
+    let count = val;
     count = count > 20 ? 20 : count;
-    lengthup.value = count
+    lengthup.value = count;
     return false;
   });
 });
+
+function selectText(id) {
+  var sel, range;
+  var el = document.getElementById(id); //get element id
+  if (window.getSelection && document.createRange) {
+    //Browser compatibility
+    sel = window.getSelection();
+    if (sel.toString() == "") {
+      //no text selection
+      window.setTimeout(function () {
+        range = document.createRange(); //range object
+        range.selectNodeContents(el); //sets Range
+        sel.removeAllRanges(); //remove all ranges from selection
+        sel.addRange(range); //add Range to a Selection.
+      }, 1);
+    }
+  } else if (document.selection) {
+    //older ie
+    sel = document.selection.createRange();
+    if (sel.text == "") {
+      //no text selection
+      range = document.body.createTextRange(); //Creates TextRange object
+      range.moveToElementText(el); //sets Range
+      range.select(); //make selection.
+    }
+  }
+}
